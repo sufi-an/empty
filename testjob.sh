3390bc93f17d4fb2e3e8e8ba14b5f39f74c3af24
sufi-an <asufian917@gmail.com>
2023-09-18 09:36:30 +0600

    commit backup

---
+++ testjob.sh
@@ -0,0 +1,12 @@
+
+
+#!/bin/bash
+
+#cd /home/d3c0d3/personal/gitBack/
+
+date >> README.md
+
+git   add .
+git  commit -m'commit backup'
+git   push origin main
+#echo "Hello World" >> ~/Desktop/gitbkup.txt 
