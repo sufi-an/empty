6d788a5a1a3297c444ca5aa6863d665077d96e08
sufi-an <asufian917@gmail.com>
2023-09-18 14:29:37 +0600

    Commit for 2023-09-18 14:29:37

--- testjob.sh
+++ testjob.sh
@@ -10,8 +10,8 @@ echo $date >> README.md
 #git add .;
 #git commit -m"${message}";
 git commit -am "${message}" && git push origin main
-#status="$(git status --branch --porcelain)";
-#echo $status >> ~/Desktop/cron_echo.txt;
+status="$(git status --branch --porcelain)";
+echo $status >> ~/Desktop/cron_echo.txt;
 #if [ "$status" == "## main...origin/main" ]; then
 #  echo "IT IS CLEAN" >> ~/Desktop/cron_echo.txt;
 #else
