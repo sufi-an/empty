d2892dea1557aabb341479b62c7f38cca4b85e90
sufi-an <asufian917@gmail.com>
2023-09-18 14:27:55 +0600

    Commit for 2023-09-18 14:27:55

--- testjob.sh
+++ testjob.sh
@@ -7,19 +7,20 @@ date=$(date +"%Y-%m-%d %T")
 message="Commit for $date"
 cd /home/d3c0d3/personal/gitBack
 echo $date >> README.md
-git add .;
-git commit -m"${message}";
-status="$(git status --branch --porcelain)";
-echo $status >> ~/Desktop/cron_echo.txt;
-if [ "$status" == "## main...origin/main" ]; then
-  echo "IT IS CLEAN" >> ~/Desktop/cron_echo.txt;
-else
-  pushStatus="$(git push  --porcelain)";
-  if ["$pushStatus" == *"Done"*];then
-    res="${pushStatus} Success"
-    echo $res >> ~/Desktop/cron_echo.txt
-  else
-    res1="${pushStatus} Failed"
-    echo $res1 >> ~/Desktop/cron_echo.txt
-  fi
-fi
+#git add .;
+#git commit -m"${message}";
+git commit -am "${message}" && git push origin main
+#status="$(git status --branch --porcelain)";
+#echo $status >> ~/Desktop/cron_echo.txt;
+#if [ "$status" == "## main...origin/main" ]; then
+#  echo "IT IS CLEAN" >> ~/Desktop/cron_echo.txt;
+#else
+#  pushStatus="$(git push  --porcelain)";
+#  if ["$pushStatus" == *"Done"*];then
+#    res="${pushStatus} Success"
+#    echo $res >> ~/Desktop/cron_echo.txt
+#  else
+#    res1="${pushStatus} Failed"
+#    echo $res1 >> ~/Desktop/cron_echo.txt
+#  fi
+#fi
