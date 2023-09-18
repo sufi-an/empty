3c9efa894bf66959a1b149d2f0461c08fd6ebddf
sufi-an <asufian917@gmail.com>
2023-09-18 10:55:01 +0600

    commit backup $dt

--- testjob.sh
+++ testjob.sh
@@ -7,5 +7,13 @@ cd /home/d3c0d3/personal/gitBack/
 date >> README.md
 
 git add .
-git commit -m'commit backup $dt' && git push -u origin main
+git commit -m'commit backup $dt' 
+
+if git push
+then
+  echo "git push succeeded"
+else
+  echo "git push failed"
+fi
+# git push -u origin main
 echo "Hello World $dt" >> ~/Desktop/gitbkup.txt 
