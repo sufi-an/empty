1e8f3f7eca9000fd5cbb45bd7d8117a2957c5987
sufi-an <asufian917@gmail.com>
2023-09-18 14:18:08 +0600

    Commit for 2023-09-18 14:18:08

--- testjob.sh
+++ testjob.sh
@@ -7,17 +7,17 @@ date=$(date +"%Y-%m-%d %T")
 message="Commit for $date"
 cd /home/d3c0d3/personal/gitBack
 echo $date >> README.md
-git add .
-git commit -m"${message}"
-status="$(git status --branch --porcelain)"
-echo $status >> ~/Desktop/cron_echo.txt
+git add .;
+git commit -m"${message}";
+status="$(git status --branch --porcelain)";
+echo $status >> ~/Desktop/cron_echo.txt;
 if [ "$status" == "## main...origin/main" ]; then
-  echo "IT IS CLEAN" >> ~/Desktop/cron_echo.txt
+  echo "IT IS CLEAN" >> ~/Desktop/cron_echo.txt;
 else
-  pushStatus="$(git push  --porcelain)"
-  #if ["$pushStatus" == "Done"];then
-  echo "$pushStatus $pwd" >> ~/Desktop/cron_echo.txt
-  #else
-   # echo "Failed" >> ~/Desktop/cron_echo.txt
+  pushStatus="$(git push  --porcelain)";
+  if ["$pushStatus" == "Done"];then
+    echo" $pushStatus" >> ~/Desktop/cron_echo.txt
+  else
+    echo "Failed" >> ~/Desktop/cron_echo.txt
  # fi
 fi
