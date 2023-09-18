6665d01cd371644f78db8ae6f469a2d61565ea24
sufi-an <asufian917@gmail.com>
2023-09-18 11:00:01 +0600

    commit backup $dt

--- testjob.sh
+++ testjob.sh
@@ -11,9 +11,9 @@ git commit -m'commit backup $dt'
 
 if git push
 then
-  echo "git push succeeded"
+  echo "git push succeeded $dt" >> ~/Desktop/gitbkup.txt
 else
-  echo "git push failed"
+  echo "git push failed $dt" >> ~/Desktop/gitbkup.txt
 fi
 # git push -u origin main
-echo "Hello World $dt" >> ~/Desktop/gitbkup.txt 
+#echo "Hello World $dt" >> ~/Desktop/gitbkup.txt 
