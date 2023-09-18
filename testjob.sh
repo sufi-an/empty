2ad0514373d546d1968e134d4d075dc9dc13cdbc
sufi-an <asufian917@gmail.com>
2023-09-18 10:34:02 +0600

    commit backup $date

--- testjob.sh
+++ testjob.sh
@@ -7,6 +7,6 @@ cd /home/d3c0d3/personal/gitBack/
 date >> README.md
 
 git add .
-git commit -m'commit backup'
+git commit -m'commit backup $date'
 git push
-echo "Hello World" >> ~/Desktop/gitbkup.txt 
+echo "Hello World $date" >> ~/Desktop/gitbkup.txt 
