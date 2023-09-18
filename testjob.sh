58157b6baa3f5b9b8311876ba3ed74e1b2bd8ff5
sufi-an <asufian917@gmail.com>
2023-09-18 14:36:04 +0600

    Commit for 2023-09-18 14:36:01

--- testjob.sh
+++ testjob.sh
@@ -7,11 +7,22 @@ date=$(date +"%Y-%m-%d %T")
 message="Commit for $date"
 cd /home/d3c0d3/personal/gitBack
 echo $date >> README.md
+#####
+
+git remote set-url origin git@github.com:sufi-an/empty.git
+git fetch
+#git branch -vv
+git branch -u origin/main
+git add .;
+git commit -m"${message}";
+git push
+#####
+
 #git add .;
 #git commit -m"${message}";
-git commit -am "${message}" && git push origin main
-status="$(git status --branch --porcelain)";
-echo $status >> ~/Desktop/cron_echo.txt;
+#git commit -am "${message}" && git push origin main
+#status="$(git status --branch --porcelain)";
+#echo $status >> ~/Desktop/cron_echo.txt;
 #if [ "$status" == "## main...origin/main" ]; then
 #  echo "IT IS CLEAN" >> ~/Desktop/cron_echo.txt;
 #else
